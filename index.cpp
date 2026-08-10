b4f7bfe88a2f9eb986f1342a29264fcdcc80a68f
Puneeth R <puneethraj107@gmail.com>
2026-08-10 14:42:15 +0530

    logical operators and arithemetic operators

---
+++ index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a ,b;
+    cout<<"enter the number a = ";
+    cin>>a;
+    cout<<"enter the number b = ";
+    cin>>b;
+    cout<<"sum = "<<(a+b)<<"\n";
+    cout<<"difference = "<<(a-b)<<"\n";
+    cout<<"multiplication = "<<(a*b)<<"\n";
+    cout<<"division = "<<(a/b)<<"\n";
+    cout<<"pre-incerment operator "<<(++a)<<"\n";
+    cout<<"pre-decrement operator "<<(--a)<<"\n";
+    return 0;
+}
