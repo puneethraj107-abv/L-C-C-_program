687dc9e525b169c0f27e9e1437a17259599473d9
Puneeth R <puneethraj107@gmail.com>
2026-08-21 00:48:41 +0530

    while loop/did a challenge to count average of n no. of students

--- index.cpp
+++ index.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int n;
-    cout<<"ternary statment"<<"\n";
-    cout<<"enter the number: "<<"\n";
-    cin>>n;
-    cout<<(n>=0 ? "positive" : "negative") <<"\n";
+int main()
+{
+    int count=10;
+    for(int i=1;i<=count;i++){
+        cout<<i<<"\n";
+    }
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -2,17 +2,14 @@
 using namespace std;
 int main()
 {
-    int marks;
-    cout<<"enter the students marks: "<<"\n";
-    cin>>marks;
-    if (marks>=90) {
-        cout<<"A"<<"\n";
-    } else if (marks>=70){
-        cout<<"B"<<"\n";
-    } else if (marks>=50){
-        cout<<"c"<<"\n";
-    } else {
-        cout<<"well whatever your grade is it's insignificant\n";
+    int no_students,marks,sum=0,average;
+    cout<<"enter the number of students"<<"\n";
+    cin>>no_students;
+    for(int i=1;i<=no_students;i++){
+        cout<<"\n\nenter the marks: ";
+        cin>>marks;
+        cout<<"\nSum: "<<(sum+=marks);
+        cout<<"\nAverage: "<<(average=sum/i);
     }
     return 0;
 }
