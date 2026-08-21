b36fc07da11709ef5161c56236941d6b09e0e016
Puneeth R <puneethraj107@gmail.com>
2026-08-22 00:29:16 +0530

    challenge to find to sum of n numbers which are divisible by 3

--- main.cpp
+++ main.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 int main()
 {
-    int n;
-    bool isprime=true;
-    cout<<"enter the number = "<<"\n";
+    int n,sum=0;
+    bool div_3=false;
+    cout<<"enter the number to which you want the sum for: "<<"\n";
     cin>>n;
-    for (int i=2;i<=n-1;i++) {
-    if (n%i==0) {
-        isprime=false;
-        break;
+    for (int i=1;i<=n;i++) {
+        if (i%3==0) {
+            sum+=i;
+
+        }
+
+
     }
-}
-    if(isprime==false){
-        cout<<"This is a non prime number"<<"\n";
-    } else {
-    cout<<"This is a prime number"<<"\n";
-    }
+    cout<<"sum: "<<sum;
     return 0;
 }
 //2147483647 - largest know prime number
