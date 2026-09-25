d636fcabcc00e90ec0d70190e3abb0d3cccfc9d0
Puneeth R <puneethraj107@gmail.com>
2026-09-25 15:44:27 +0530

    10 string operation in c

--- hello.c
+++ hello.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-
+#include <conio.h>
+#include <string.h>
 int main()
 {
-    int num, orgnum;
-    int sum=0,rev=0,rem;
-    printf("enter a number\n");
-    scanf("%d",&num);
-    orgnum=num;
-    while (num>0) {
-        rem=num%10;
-        rev=rev*10+rem;
-        num/=10;
-    }
-    printf("\n reversed number=%d",rev);
-    if(orgnum==rev)
-        printf("\n number is a palindrome");
-    else
-        printf("\n number is not a palindrome");
-    return 0;
-
+char str1[50]="Bengaluru";
+char str2[50]="Nagara";
+printf("\n string1=%s string2=%s",str1,str2);
+printf("\n 1.length of the %s=%d",str1,strlen(str1));
+printf("\n 2.string copy in str2=%s",strcpy(str2,"city"));
+printf("\n 3.concatenation = %s",strcat(str1,str2));
+printf("\n 4.compare str1 & str2 %d",strcmp(str1,str2));
+printf("\n 5.string in lower case %s",strlwr(str2));
+printf("\n 6.string in upper case %s",strupr(str2));
+printf("\n 7.substring search %s",strchr(str1,'n'));
+printf("\n 8.Duplicate string=%s",strdup(str1));
+printf("\n 9.string reverse=%s",strrev(str2));
+printf("\n 10.set all characters to #=%s",strset(str1,'#'));
+return 0;
 }
